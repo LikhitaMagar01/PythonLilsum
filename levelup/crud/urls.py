6cf2671b6ae6cf4ce91dae8d6ca80162f6b32541
LikhitaMagar01 <leekhitame@gmail.com>
2024-01-28 13:50:00 +0545

    fix: crud in template

--- levelup/crud/urls.py
+++ levelup/crud/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from crud.views import list_all_user, detail_view_user
-
+from crud.views import (list_all_user, detail_view_user, 
+                        create_user_info, update_user_info, 
+                        delete_user_info
+                        )
+app_name = 'crud'
 urlpatterns = [
-    path('list/', list_all_user),
-    path('detail/<int:user_id>/', detail_view_user)
+    #{% url 'crud:list' %}
+    path('list/', list_all_user, name='list'),
+    path('detail/<int:user_id>/', detail_view_user, name='detail'),
+    path('create/', create_user_info, name='create'),
+    path('update/<int:user_id>', update_user_info, name='update'),
+    path('delete/<int:user_id>', delete_user_info, name='delete')
 ]
